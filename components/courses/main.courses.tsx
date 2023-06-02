0828c530d83ac215dfafe19126aa3345f06df525
Prosecutor1x <krishnarjunnanda@gmail.com>
2023-06-02 12:53:08 +0530

    Courses Added

---
+++ components/courses/main.courses.tsx
@@ -0,0 +1,93 @@
+import { MinusIcon, AddIcon } from "@chakra-ui/icons";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+} from "@chakra-ui/react";
+import React from "react";
+
+// interface ICourses {
+//   category: string;
+//   first: string;
+//   second: string;
+//   third: string;
+//   fourth: string;
+//   fifth: string;
+//   sixth: string;
+// }
+const CoursesData = [
+  {
+    category: "Classes 3 to 5",
+    first: "Machine and Mechanisms",
+    second: "Robotics",
+    third: "IOT and 3D designs",
+    fourth: "Website Developement",
+    fifth: "",
+    sixth: "",
+  },
+  {
+    category: "Classes 6 to 8",
+    first: "App Developement",
+    second: "Learn to build and fly Drones and RC planes",
+    third: "IOT and Arduino",
+    fourth: "Artificial Intelligience",
+    fifth: "Programming with Python and Java",
+    sixth: "Virtual Reality",
+  },
+  {
+    category: "Classes 9 and above",
+    first: "Programming with Python and Javascript",
+    second: "Robotics",
+    third: "Learn to build and fly Drones and RC planes",
+    fourth: "Artificial Intelligience",
+    fifth: "Combat Robotics",
+    sixth: "",
+  },
+];
+
+console.log(CoursesData);
+const Courses = () => {
+  return (
+    <div className=" text-black space-y-8 my-6 flex flex-col justify-center pb-32 pt-16 lg:pt-0 font-quicksand" id="courses">
+      <h1 className="text-center text-3xl font-semibold my-4 "> Courses</h1>
+      <>
+        {CoursesData.map((data, id) => {
+          return(
+            <Accordion key={id} className="rounded-xl border " allowMultiple>
+            <AccordionItem className="rounded-xl border  ">
+              <h2>
+                <AccordionButton _expanded={{ bg:"#2f4f4f " , color: "white" }} className="rounded-xl bg-slate-300">
+                  <Box as="span" flex="1"  className="text-center font-semibold p-12 text-xl ">
+                  🧑‍🏫 {data.category} ✨⏰
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel>
+                <div className="text-xl font-semibold">
+
+                <h1>📱{data.first}</h1>
+                <h1>🤖{data.second}</h1>
+                <h1>🔥{data.third}</h1>
+                <h1>❤️{data.fourth}</h1>
+                {data.fifth && <h1>🧸{data.fifth}</h1>}
+                {data.sixth && <h1>🛬 {data.sixth}</h1>}
+                </div>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+         
+            
+          )
+          
+        })}
+      </>
+      
+    </div>
+  );
+};
+
+export default Courses;
--- components/hero/main.hero.tsx
+++ components/hero/main.hero.tsx
@@ -6,23 +6,23 @@ import Demo from "../demo/Demo.main";
 
 export default function Hero () {
   return (
-    <div className="flex h-screen justify-center items-center bg-white px-9">
-      <div className="w-2/3 flex flex-col justify-center items-center">
-        <img src="/shape3.png" className="mt-10 " />
+    <div className="lg:flex h-screen justify-center items-center bg-white px-9 gap-20">
+      <div className="lg:w-2/3 flex flex-col justify-center items-center">
+        <img src="/logo.png" className="mt-10 " />
 
-        <h1 className="text-3xl font-semibold text-font1">
+        <h1 className="lg:text-3xl font-semibold text-font1">
           Hello welcome to BlackBird RobotiX
         </h1>
         <a
-          href="https://github.com/Rajdip019"
+          href=""
           target="_blank"
-          className=" text-font1 mt-10 "
+          className=" text-font1 lg:mt-6 "
           rel="noreferrer"
         >
           -by BlackBird India
         </a>
       </div>
-      <div className="w-3/5">
+      <div className="lg:w-3/5 mt-12 lg:mt-0">
         <Demo/>
       </div>
     </div>
