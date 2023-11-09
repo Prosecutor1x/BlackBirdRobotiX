f48b44e3d23fefe2b404a9b29af82bd8ed8dc28f
Prosecutor1x <krishnarjunnanda@gmail.com>
2023-11-09 18:41:23 +0530

    changesd

--- components/events/register.event.tsx
+++ components/events/register.event.tsx
@@ -43,7 +43,7 @@ const Register = () => {
 
   const fileTypes = ["SB3"];
   const targetDate = "2023-11-05T23:59:59";
-  const targetDate2 = "2023-11-4T23:59:59";
+  const targetDate2 = "2023-11-04T23:59:59";
   const [disable, setDisable] = useState<boolean>(false);
   const [disable2, setDisable2] = useState<boolean>(false);
 
@@ -56,11 +56,13 @@ const Register = () => {
     if (timeDifference > 0) {
       setDisable(true);
     } else if (timeDifference < -86400000) {
-      setDisable(true);
+      setDisable(false);
     } else {
       setDisable(false);
     }
   };
+
+  console.log(disable);
   const handledisableRegistration = () => {
     const now = new Date();
     const target = new Date(targetDate2);
